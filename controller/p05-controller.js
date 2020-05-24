e1c212baabb9156bac6e9481c54512d2c6e183df
gclou <clo15008@byui.edu>
2020-05-23 23:13:03 -0600

    prove 5

--- controller/p05-controller.js
+++ controller/p05-controller.js
@@ -1,3 +1,5 @@
+const User = require('../data/user');
+
 exports.setCookie = (req, res, next) => {
     res.setHeader('Set-Cookie', 'style=true');
 
@@ -10,6 +12,51 @@ exports.changeColor = (req, res, next) => {
     
         console.log(req.body);
 }
+
+//   /p05/signUp
+exports.signUp = (req, res, next) => {
+    res.render('pages/p05-signup', { 
+        title: 'Prove 05', 
+        path: '/p05-signup', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+}
+
+//   /p05/addUser
+exports.addUser = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email})
+    .then(userDoc => {
+        if(userDoc){
+            return res.redirect('/p05/p05-signup');
+            //alert("email taken");
+            //redirection message
+        }
+        const user = new User({
+            email: email,
+            password: password
+            //cart: { items: [] }
+        });
+        return user.save();
+
+    })
+    .then(result => {
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    //const confirmPassword = req.body.confirmPassword;
+
+}
+
+//   /p05/authenticate
+
+
+
+
 
 // exports.getProducts = (req, res, next) => {
 //     Product.find()
--- routes/p05.js
+++ routes/p05.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/p05-controller');
 
-// router.get('/',(req, res, next) => {
-//     res.render('pages/p04.ejs', { 
-//         title: 'Prove 04', 
-//         path: '/p04', // For pug, EJS 
-//         activeTA03: true, // For HBS
-//         contentCSS: true, // For HBS
-//     });
-// });
+
 
 router.get('/',(req, res, next) => {
     const backgroundColor = "background-color:" + req.get('Cookie').split(';')[1].trim().split('=')[1];
@@ -21,14 +14,12 @@ router.get('/',(req, res, next) => {
     });
 });
 
-// router.post('/change-style',(req, res, next) => {
-//     res.setHeader('Set-Cookie', req.body);
-//     res.setHeader('Set-Cookie', 'counter=0');
-//     res.redirect('/');
+router.get('/p05-signup', controller.signUp);
 
-//     console.log(req.body);
-// });
+//   /p05/addUser
+router.post('/addUser', controller.addUser);
 
+//   /p05/authenticate
 router.post('/change-style', controller.changeColor);
 
 module.exports = router;
